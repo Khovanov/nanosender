3d16245324e29440b94db356fe26270910d3dd09
Khovanov <ubuntu@rails-dev-box>
2017-03-23 08:15:14 +0300

    create model User

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  User.create(
+    [
+      {
+        id: 1,
+        name: "Petr"
+      },
+      {
+        id: 2,
+        name: "Andy"
+      },
+      {
+        id: 3,
+        name: "Clark"
+      },
+      {
+        id: 4,
+        name: "Artem"
+      }
+    ]
+  )
+
+  # SELECT setval('your_table_id_seq', (SELECT MAX(id) FROM your_table));
+  # ActiveRecord::ConnectionAdapters::PostgreSQL::SchemaStatements#reset_pk_sequence!
+  # reset_pk_sequence!(table, pk = nil, sequence = nil)
+  ActiveRecord::Base.connection.reset_pk_sequence!('users')
+  # ActiveRecord::Base.connection.tables
